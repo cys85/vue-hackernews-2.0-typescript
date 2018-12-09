120278eec4d96aa011b59a3c43d4c0dfda9d0e3f
cys85 <kuerbb@aliyun.com>
2018-12-10 00:26:05 +0800

    up

--- src/views/Home.view.vue
+++ src/views/Home.view.vue
@@ -5,12 +5,22 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '../components/HelloWorld.vue'; // @ is an alias to /src
+import { getTest } from '@/api';
+import { of, Observable } from 'rxjs';
 
 @Component({
   // components: {
   //   HelloWorld,
   // },
   asyncData() {
+    const toPromise = (obs: any) =>
+      new Promise((resolve, reject) => {
+        obs.subscribe({
+          complete: resolve,
+          error: reject
+        });
+      });
+    console.log(toPromise(of(() => getTest())))
     return Promise.resolve([]);
   },
 })
